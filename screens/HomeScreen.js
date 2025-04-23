751f3332b1eff856619005d66d1583ea033ec976
seojoonrp <seojoonrp@snu.ac.kr>
2025-04-23 17:32:13 +0900

    seperated babyonbed component, css refactoring finished

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,37 +2,24 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import BabyOnBed from '../components/BabyOnBed.js';
+
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
     <View style={Styles.container}>
-      <View style={Styles.bar} />
-      <View style={Styles.pinkBackground}>
-        <TouchableOpacity
-          style={Styles.startButton}
-          onPress={() => navigation.navigate('Phase1GameScreen')}
-        >
-          <Text style={Styles.buttonText}>START</Text>
-        </TouchableOpacity>
-        <Image
-          style={[Styles.image, { top: 42, left: 32 }]}
-          source={require("../assets/images/pillow.png")}
-        />
-        <Image
-          style={[Styles.image, { top: 212, left: 2 }]}
-          source={require("../assets/images/blanket.png")}
-        />
-        <Image
-          style={[Styles.image, { top: 48, left: 55 }]}
-          source={require("../assets/images/baby.png")}
-        />
-        <Image
-          style={[Styles.image, { top: 225, left: 47 }]}
-          source={require("../assets/images/title.png")}
-        />
-      </View>
-      <View style={Styles.bar} />
+      <BabyOnBed isClickable={false} />
+      < Image
+        style={Styles.titleImage}
+        source={require("../assets/images/title.png")}
+      />
+      <TouchableOpacity
+        style={Styles.startButton}
+        onPress={() => navigation.navigate('Phase1GameScreen')}
+      >
+        <Text style={Styles.buttonText}>START</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -47,34 +34,16 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  bar: {
-    backgroundColor: '#F7C89C',
-    width: 361,
-    height: 20,
-    borderRadius: 10,
-    zIndex: 2
-  },
-  pinkBackground: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: '#FEE5E5',
-    width: 361,
-    height: 566,
-    borderRadius: 13,
-    marginVertical: -10
-  },
-  image: {
-    position: 'absolute',
-    zIndex: 1,
+  titleImage: {
+    width: 279,
+    marginTop: 105,
   },
   startButton: {
     justifyContent: 'center',
     alignItems: 'center',
     paddingHorizontal: 39,
     paddingVertical: 6,
-    marginTop: 394,
+    marginTop: 10,
     backgroundColor: '#F8DCAF',
     borderColor: 'white',
     borderWidth: 3,
@@ -89,14 +58,4 @@ const Styles = StyleSheet.create({
     fontWeight: 400,
     fontStyle: 'normal',
   }
-  // title: {
-  //   marginTop: 100,
-  //   color: '#F8DCAF',
-  //   textAlign: 'center',
-  //   fontSize: 80,
-  //   fontFamily: 'MurukMuruk',
-  //   fontWeight: 400,
-  //   lineHeight: 80,
-  //   zIndex: 2,
-  // },
 })
